f0acabc92bae0d4d6abd22ded0745e45a4df1da9
Jenner-Maxim <jennersi1remaxim@gmail.com>
2023-04-16 08:27:24 -0500

    Search API, search front-end for the MovieLand website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
-import React from "react";
+import { useEffect } from "react";
+// 6a1a1a30
+
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+const API_URL = "http://omdbapi.com?apikey=6a1a1a30";
+
+const movie1 = {
+  Title: "The Amazing Spiderman 2 Webb Cut",
+  Year: "2021",
+  imdbID: "tt18351128",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg",
+};
 
 const App = () => {
-  return <h1>App</h1>;
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&S=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : 'https://via.placeholder.com/400'
+              }
+              alt={movie1.Title}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
