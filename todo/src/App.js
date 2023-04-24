5754fc81f69d236998f53c55f016346a2a0a5c4f
JennerMaxim <jennersi1remaxim@gmail.com>
2023-04-24 15:38:01 +0300

    Font-End and Add Todo Action

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import { useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useEffect } from "react";
+import TodoList from "./TodoList";
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function handleAddTo(e) {
+    const name = todoNameRef.current.value;
+    if (name === "") return;
+    setTodos((prevTodo) => {
+      return [...prevTodo, { id: uuidv4(), name: name, complete: false }];
+    });
+    todoNameRef.current.value = null;
+  }
   return (
-    <div>Maxim</div>
-  )
+    <>
+      <TodoList todos={todos} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTo}>Add Todo</button>
+      <button>Clear Completed</button>
+      <div>0 left</div>
+    </>
+  );
 }
 
 export default App;
